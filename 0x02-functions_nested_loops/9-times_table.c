fea8599885cae1afe31058a713891934459792c0
chiamakaumoh <chiamaumoh@gmail.com>
2022-07-10 16:26:42 -0700

    Task 104

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,7 @@ int i, j k;
 
 for (i = 0; i < 10; i++)
 {
-for (l = 0; j < 10; j++)
+for ('l' = 0; j < 10; j++)
 {
 k = j * i;
 if (j == 0)
@@ -19,14 +19,14 @@ _putchar(k + '0');
 if (k < 10 && j != 0)
 {
 _putchar(',');
-_putchar('');
-_putchar('');
+_putchar(' ');
+_putchar(' ');
 _putchar(k + '0');
 }
 else if (k >= 10)
 {
-_putchar('');
-_putchar('');
+_putchar(' ');
+_putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
 }
