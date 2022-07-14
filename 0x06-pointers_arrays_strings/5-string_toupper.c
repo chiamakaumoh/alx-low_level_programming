c87b855d9a6a89b07d96e346731b666c89f89991
chiamakaumoh <chiamaumoh@gmail.com>
2022-07-14 16:54:23 -0700

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,14 +7,17 @@
  *
  * Return: the resulting string
  */
-char *string_youpper(char *s)
+char *string_toupper(char *s)
 {
 int i;
 
 for (i = 0; s[i] != '\0'; i++)
 {
 if (s[i] >= 'a' && s[i] <= 'z')
+
 s[i] = s[i] - 32;
+
 }
+
 return (s);
 }
